16eb5375826755936914ade8c428356c6a838495
Pavel <pashalevy@gmail.com>
2022-11-13 16:48:05 +0100

    Addeded Accordion Project

---
+++ projects/accordion-project/script.js
@@ -0,0 +1,11 @@
+// Variable
+
+const accordion = document.getElementsByClassName('content-container');
+
+console.log(accordion);
+
+for (let i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+  });
+}
